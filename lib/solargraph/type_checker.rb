36c7e63706acb11a8c8098302ae6576c6b61f280
Vince Broz <vince@broz.cc>
2025-03-26 09:34:28 -0400

    Fix merge issue

--- lib/solargraph/type_checker.rb
+++ lib/solargraph/type_checker.rb
@@ -333,7 +333,7 @@ module Solargraph
                   next
                 else
                   ptype = params.key?(par.name) ? params[par.name][:qualified] : ComplexType::UNDEFINED
-                  ptype = ptype.self_to(par.context.namespace)
+                  ptype = ptype.self_to_type(par.context)
                   if ptype.nil?
                     # @todo Some level (strong, I guess) should require the param here
                   else
