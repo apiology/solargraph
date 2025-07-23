e10fc3e4a8a05129abd1548323584bbc125e17f2
Vince Broz <vince@broz.cc>
2025-07-23 06:55:32 -0400

    Fix merge issue

--- lib/solargraph/logging.rb
+++ lib/solargraph/logging.rb
@@ -18,7 +18,6 @@ module Solargraph
               warn "Invalid value for SOLARGRAPH_LOG: #{configured_level.inspect} - valid values are #{LOG_LEVELS.keys}" if configured_level
               DEFAULT_LOG_LEVEL
             end
-    # @sg-ignore Fix cvar issue
     @@logger = Logger.new(STDERR, level: level)
     # @sg-ignore Fix cvar issue
     @@logger.formatter = proc do |severity, datetime, progname, msg|
