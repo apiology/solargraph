1000778414d69ea41da4b5824aef6c19eee155f4
Vince Broz <vince@broz.cc>
2025-09-10 16:59:09 -0400

    Update spec expectations

--- spec/api_map_method_spec.rb
+++ spec/api_map_method_spec.rb
@@ -136,10 +136,10 @@ describe 'Solargraph::ApiMap methods' do
   describe '#cache_all_for_doc_map!' do
     it 'can cache gems without a bench' do
       api_map = Solargraph::ApiMap.new
-      doc_map = instance_double(Solargraph::DocMap, cache_all!: true)
+      doc_map = instance_double(Solargraph::DocMap, cache_doc_map_gems!: true)
       allow(Solargraph::DocMap).to receive(:new).and_return(doc_map)
       api_map.cache_all_for_doc_map!($stderr)
-      expect(doc_map).to have_received(:cache_all!).with($stderr)
+      expect(doc_map).to have_received(:cache_doc_map_gems!).with($stderr)
     end
   end
 
