583e66dbbd8a6f62ba17db7b6530c6b7a1c8354a
Vince Broz <vince@broz.cc>
2025-09-12 20:26:08 -0400

    Small type fix

--- lib/solargraph/doc_map.rb
+++ lib/solargraph/doc_map.rb
@@ -426,6 +426,7 @@ module Solargraph
           end.compact
         else
           Solargraph.logger.warn "Failed to load gems from bundle at #{workspace&.directory}: #{e}"
+          nil
         end
       end
     end
