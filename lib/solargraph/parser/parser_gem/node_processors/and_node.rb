36aceae520af36d7910eb05c043bfbbe5e2c75e4
Vince Broz <vince@broz.cc>
2026-01-03 18:32:39 -0500

    rubocop -a

--- lib/solargraph/parser/parser_gem/node_processors/and_node.rb
+++ lib/solargraph/parser/parser_gem/node_processors/and_node.rb
@@ -12,7 +12,9 @@ module Solargraph
 
             position = get_node_start_position(node)
             # @sg-ignore https://github.com/castwide/solargraph/pull/1114
-            enclosing_breakable_pin = pins.select{|pin| pin.is_a?(Pin::Breakable) && pin.location.range.contain?(position)}.last
+            enclosing_breakable_pin = pins.select do |pin|
+              pin.is_a?(Pin::Breakable) && pin.location.range.contain?(position)
+            end.last
             FlowSensitiveTyping.new(locals, enclosing_breakable_pin).process_and(node)
           end
         end
